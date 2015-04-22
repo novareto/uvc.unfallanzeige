6c96312520f4df5d5578629a73f762702bbe8f83
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2015-04-22 09:59:35 +0000

    chagnes

--- uvc.unfallanzeige/trunk/uvc/unfallanzeige/static/step4.js
+++ uvc.unfallanzeige/trunk/uvc/unfallanzeige/static/step4.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
+ 
+ $('input#form-accidenti-field-unfdatum').attr('placeholder', 'TT.MM.JJJJ').mask('99.99.9999');
+ $('input#form-accidenti-field-unfzeit').attr('placeholder', 'HH:MM').mask('99:99');
  $('#field-form-accidenti-field-unfzeit').appendFieldTo('#field-form-accidenti-field-unfdatum');
 });
