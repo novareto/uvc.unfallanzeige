dcceabce6a1985e84d0284503c7a26d91c1fb151
ck <ck@novareto.de>
2015-04-22 09:59:35 +0000

    chagnes

--- uvc/unfallanzeige/static/uaz.js
+++ uvc/unfallanzeige/static/uaz.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
  $('input#form-action-speichern, input#form-action-weiter').addClass('btn-primary');
  $('input#form-job-field-uadst').attr('placeholder', 'MM.JJJJ').mask('99.9999');
  $('input#form-person-field-prsgeb').attr('placeholder', 'TT.MM.JJJJ').mask('99.99.9999');
- $('input#form-accidenti-field-unfdatum').attr('placeholder', 'TT.MM.JJJJ').mask('99.99.9999');
- $('input#form-accidenti-field-unfzeit').attr('placeholder', 'HH:MM').mask('99:99');
  $('input#form-accidentii-field-unfaedatum').attr('placeholder', 'TT.MM.JJJJ').mask('99.99.9999');
  $('input#form-accidentii-field-unfaezeit').attr('placeholder', 'HH:MM').mask('99:99');
  $('input#form-accidentii-field-unfwax').attr('placeholder', 'TT.MM.JJJJ').mask('99.99.9999');
